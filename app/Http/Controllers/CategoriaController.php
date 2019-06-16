4e27c919c4369ce042d143eb6647883ecb0b37f0
Denis Luciano <denis.lopes3012@gmail.com>
2019-06-16 17:18:40 -0300

    controllers criados+cadastro de usuario(backend)

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -34,7 +34,6 @@ class CategoriaController extends Controller
             return "sucesso";
 
 
-
         return response()->json($categoria);
 
     }
--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -2,18 +2,32 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
 
 use App\Usuario;
+use Illuminate\Support\Facades\DB;
 
 class UsuarioController extends Controller
 {
     //
     public function store(Request $request){
 
-        $usuario = new Usuario();
+        // $categorias = DB::insert('insert into categorias (titulo, prazo_max, tipo)
+        // values (requesttitulo,30,requesttipo )');
+        $cons = "insert into usuarios values ('$request->email','$request->senha','$request->cpf',
+        '$request->nome','$request->apelido', lista_enderecos(endereco('$request->estado',
+        '$request->bairro',$request->numero,'$request->cidade','$request->cep')),
+        lista_telefones('$request->numero_cel'))";
+
+        $usuario =  DB::insert($cons);
 
 
+        if($usuario)
+            return "sucesso";
+
+
+        return response()->json($usuario);
 
     }
 }
