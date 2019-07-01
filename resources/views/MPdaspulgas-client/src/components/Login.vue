d872001a0060f02443b03f7434244824a764a569
Henrique Penna <henrique.p.diniz@ufv.br>
2019-07-01 10:47:20 -0300

    logo nova

--- resources/views/MPdaspulgas-client/src/components/Login.vue
+++ resources/views/MPdaspulgas-client/src/components/Login.vue
@@ -4,28 +4,28 @@
       <v-flex xs11 sm7 md5 lg4 xl3 >
         <v-card class="elevation-12">
           <v-toolbar extended dark color="#de5d3c">
-            <v-img src="https://i.imgur.com/8Xaojp4.png" max-width="450" min-width="300" class="mt-5" aspect-ratio="4.5" contain></v-img>
+            <v-img src="https://i.imgur.com/O2kLb3y.png" max-width="450" min-width="300" class="mt-5" aspect-ratio="4.5" contain></v-img>
           </v-toolbar>
-
+          
           <v-card-text >
             <v-form v-if="registrando == false">
-              <v-text-field
-                color="#de5d3c"
-                v-model="user.email"
-                prepend-icon="person"
-                name="login"
-                label="E-mail"
+              <v-text-field 
+                color="#de5d3c" 
+                v-model="user.email" 
+                prepend-icon="person" 
+                name="login" 
+                label="E-mail" 
                 placeholder="Insira seu e-mail aqui"
-                type="text"
+                type="text" 
                 :rules="emailRules"></v-text-field>
               <v-text-field
                 color="#de5d3c"
                 v-model="user.password"
-                prepend-icon="lock"
-                name="Senha"
-                label="Senha"
+                prepend-icon="lock" 
+                name="Senha" 
+                label="Senha" 
                 placeholder="Insira sua senha aqui"
-                id="password"
+                id="password" 
                 min="5"
                 :append-icon="senha_visivel ? 'visibility' : 'visibility_off'"
                 @click:append = "() => (senha_visivel = !senha_visivel)"
@@ -38,163 +38,83 @@
             <v-form v-if="registrando == true">
               <v-layout row wrap>
               <v-flex xs12>
-              <v-text-field
-                color="#de5d3c"
-                v-model="user_cadastro.nome"
-                prepend-icon="assignment_ind"
-                name="login"
-                label="Insira seu nome completo"
+              <v-text-field 
+                color="#de5d3c" 
+                v-model="user_cadastro.nome" 
+                prepend-icon="assignment_ind" 
+                name="login" 
+                label="Insira seu nome completo" 
                 placeholder="Insira seu nome completo"
-                type="text"
+                type="text" 
                 ></v-text-field>
               </v-flex>
               <v-flex xs6>
               <v-text-field
-                color="#de5d3c"
-                v-model="user_cadastro.apelido"
-                prepend-icon="person"
-                name="login"
-                label="Insira seu apelido"
+                color="#de5d3c" 
+                v-model="user_cadastro.apelido" 
+                prepend-icon="person" 
+                name="login" 
+                label="Insira seu apelido" 
                 placeholder="Insira seu apelido"
-                type="text"
+                type="text" 
                 ></v-text-field>
               </v-flex>
               <v-flex xs6>
-              <v-text-field
-                color="#de5d3c"
-                v-model="user_cadastro.cpf"
+              <v-text-field 
+                color="#de5d3c" 
+                v-model="user_cadastro.cpf" 
                 mask="###.###.###-##"
-                prepend-icon="fingerprint"
-                name="login"
+                prepend-icon="fingerprint" 
+                name="login" 
                 label="Insira seu CPF"
                 placeholder="Insira seu CPF"
-                type="text"
+                type="text" 
                 ></v-text-field>
               </v-flex>
               <v-flex xs12>
-              <v-text-field
-                color="#de5d3c"
-                v-model="user_cadastro.email"
-                prepend-icon="alternate_email"
-                name="login"
+              <v-text-field 
+                color="#de5d3c" 
+                v-model="user_cadastro.email" 
+                prepend-icon="alternate_email" 
+                name="login" 
                 label="Insira seu e-mail"
                 placeholder="Insira seu e-mail"
-                type="text"
+                type="text" 
                 ></v-text-field>
+              </v-flex>
+
+              <v-flex xs10>
+              <v-text-field 
+                color="#de5d3c" 
+                v-model="user_cadastro.telefones" 
+                prepend-icon="phone" 
+                mask="(##) #####-####"
+                name="login" 
+                label="Insira seus telefones"
+                placeholder="Insira seus telefones"
+                type="text" 
+                ></v-text-field>
+              </v-flex>
+              <v-flex xs1 class="pt-2">
+                <v-btn flat icon color="green" @click="add_phone">
+                  <v-icon>add</v-icon>
+                </v-btn>
               </v-flex>
               <v-flex xs12>
               <v-text-field
                 color="#de5d3c"
                 v-model="user_cadastro.password"
-                prepend-icon="lock"
-                name="Senha"
+                prepend-icon="lock" 
+                name="Senha" 
                 label="Insira sua senha"
                 placeholder="Insira sua senha"
-                id="password"
+                id="password" 
                 min="5"
                 :append-icon="senha_visivel ? 'visibility' : 'visibility_off'"
                 @click:append = "() => (senha_visivel = !senha_visivel)"
                 :type="senha_visivel ? 'text' : 'password'"
                 @keyup.enter="submit()"
               ></v-text-field>
-              </v-flex>
-              <v-flex xs10 v-for="bas in user_cadastro.qnt_telefones" :key="bas">
-              <v-text-field
-                color="#de5d3c"
-                v-model="user_cadastro.telefones[bas-1]"
-                prepend-icon="phone"
-                mask="(##) #####-####"
-                name="login"
-                label="Insira seus telefones"
-                placeholder="Insira seus telefones"
-                type="text"
-                ></v-text-field>
-              </v-flex>
-              <v-flex xs1 class="pt-2">
-                <v-btn flat icon color="green" @click="add_phone(true)">
-                  <v-icon>add</v-icon>
-                </v-btn>
-              </v-flex>
-              <v-flex xs1 class="pt-2">
-                <v-btn flat icon color="red" @click="add_phone(false)">
-                  <v-icon>remove</v-icon>
-                </v-btn>
-              </v-flex>
-              <br>
-              <v-flex xs4>
-                Endereço principal
-              </v-flex>
-              <v-flex xs8 class="mt-2">
-              <v-divider></v-divider>
-              </v-flex>
-              <br><br>
-              <v-flex xs5>
-              <v-text-field
-                color="#de5d3c"
-                v-model="user_cadastro.endereco.rua"
-                class="pr-2"
-                name="rua"
-                label="Rua"
-                placeholder="Rua"
-                type="text"
-                ></v-text-field>
-              </v-flex>
-              <v-flex xs5>
-              <v-text-field
-                color="#de5d3c"
-                v-model="user_cadastro.endereco.bairro"
-                class="pr-2"
-                name="bairro"
-                label="Bairro"
-                placeholder="Bairro"
-                type="text"
-                ></v-text-field>
-              </v-flex>
-              <v-flex xs2>
-              <v-text-field
-                color="#de5d3c"
-                v-model="user_cadastro.endereco.numero"
-                class="pr-2"
-                name="numero"
-                label="Número"
-                placeholder="Nº"
-                type="text"
-                ></v-text-field>
-              </v-flex>
-              <v-flex xs4>
-              <v-text-field
-                color="#de5d3c"
-                v-model="user_cadastro.endereco.cep"
-                class="pr-2"
-                mask="#####-###"
-                name="cep"
-                label="CEP"
-                placeholder="CEP"
-                type="text"
-                ></v-text-field>
-              </v-flex>
-              <v-flex xs5>
-              <v-text-field
-                color="#de5d3c"
-                v-model="user_cadastro.endereco.cidade"
-                class="pr-2"
-                name="cidade"
-                label="Cidade"
-                placeholder="Cidade"
-                type="text"
-                ></v-text-field>
-              </v-flex>
-              <v-flex xs3>
-              <v-select
-                :items="estados"
-                color="#de5d3c"
-                v-model="user_cadastro.endereco.estado"
-                class="pr-2"
-                name="estado"
-                label="Estado"
-                placeholder="Estado"
-                type="text"
-                ></v-select>
               </v-flex>
               </v-layout>
             </v-form>
@@ -204,7 +124,7 @@
             <v-btn color="#de5d3c" v-if="registrando == false" dark @click="register(true)">Registrar-se</v-btn>
             <v-btn color="#de5d3c" v-if="registrando == false" dark @click="submit()" :loading="carregando">Fazer Login</v-btn>
             <v-btn color="#de5d3c" v-if="registrando == true" dark @click="register(false)">Voltar</v-btn>
-            <v-btn color="#de5d3c" v-if="registrando == true" dark @click="cadastra" :loading="carregando">Fazer Registro</v-btn>
+            <v-btn color="#de5d3c" v-if="registrando == true" dark @click="register(true)" :loading="carregando">Fazer Registro</v-btn>
           </v-card-actions>
         </v-card>
       </v-flex>
@@ -234,8 +154,7 @@
   const axios = require('axios');
   export default {
       data () {
-        return { 
-          estados: ['AC','GO','MG','RJ','SP'],
+        return {
           mensagem: '',
           message_box: false,
           logado: false,
@@ -250,16 +169,7 @@
             apelido: '',
             email: '',
             cpf: '',
-            qnt_telefones: 1,
-            telefones:[],
-            endereco: {
-              rua: '',
-              bairro: '',
-              estado: '',
-              numero: '',
-              cidade: '',
-              cep: '',
-            },
+            telefones: [],
             password: '',
           },
           senha_visivel: false,
@@ -278,14 +188,11 @@
         submit () {
           this.carregando = true;
           axios.post(sessionStorage.getItem('url')+'/api/autentica', this.user )
-          .then(response => {
-            console.log(response.data)
+          .then(response => { 
+
             if(response.data.ok == true){
-              sessionStorage.setItem('nome', response.data.usuario.original[0].nome)
-              sessionStorage.setItem('id', response.data.usuario.original[0].id)
-              sessionStorage.setItem('email', response.data.usuario.original[0].email)
-              sessionStorage.setItem('apelido', response.data.usuario.original[0].apelido)
-              sessionStorage.setItem('cpf', response.data.usuario.original[0].cpf)
+              sessionStorage.setItem('nome', response.data.usuario.original.nome)
+              sessionStorage.setItem('email', response.data.usuario.original.email)
               this.$router.push('/Anuncios');
             }
             else{
@@ -301,41 +208,10 @@
             if(this.mensagem == 'Error: Request failed with status code 400')
               this.mensagem = "E-mail ou senha incorreta"
             console.log(this.mensagem);
-          });
+          }); 
       },
-      cadastra () {
-          console.log(this.user_cadastro)
-          this.carregando = true;
-          axios.post(sessionStorage.getItem('url')+'/api/usuario', this.user_cadastro )
-          .then(response => {
-            console.log(response.data)
-            sessionStorage.setItem('nome', response.data.usuario.original[0].nome)
-            sessionStorage.setItem('id', response.data.usuario.original[0].id)
-            sessionStorage.setItem('email', response.data.usuario.original[0].email)
-            sessionStorage.setItem('apelido', response.data.usuario.original[0].apelido)
-            sessionStorage.setItem('cpf', response.data.usuario.original[0].cpf)
-            this.$router.push('/Anuncios');
-          })
-          .catch(error => {
-            this.message_box = true;
-            this.mensagem = error;
-            this.carregando = false;
-            if(this.mensagem == 'Error: Request failed with status code 400')
-              this.mensagem = "E-mail ou senha incorreta"
-            console.log(this.mensagem);
-          });
-      },
-      add_phone(x){
-        if(x == true)
-          this.user_cadastro.qnt_telefones = this.user_cadastro.qnt_telefones + 1;
-        else{
-          if(this.user_cadastro.qnt_telefones > 0){
-            this.user_cadastro.telefones[this.user_cadastro.qnt_telefones-1] = null;
-            this.user_cadastro.telefones.splice(this.user_cadastro.qnt_telefones-1, 1); 
-            if(this.user_cadastro.qnt_telefones > 1)
-              this.user_cadastro.qnt_telefones = this.user_cadastro.qnt_telefones - 1;
-          }
-        }
+      add_phone(){
+
       },
       register(x){
         if(x == true)
